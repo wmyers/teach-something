dc087fd37340fdaa4c5fce672cc49f4ae3cac4aa
hotmail <wmmyers@hotmail.com>
2014-07-17 23:42:23 +0800

    now preventing duplicates of question model and view seperately - should be possible to have model logic prevent view duplication

--- www/app/views/Question.js
+++ www/app/views/Question.js
@@ -15,7 +15,8 @@ define(function (require) {
     return Backbone.View.extend({
 
         events: function() {
-            var selector = '#next-' + this.model.get('id');
+            //reference unique id for selector (prevents multiple event firing)
+            var selector = '#question-' + this.model.get('id') + " a.next";
             var ev = {};
             ev['click ' + selector] = 'next';
             return ev;
@@ -26,8 +27,10 @@ define(function (require) {
         },
 
         render: function () {
-            //TODO remove if already there
-            this.$el.append(template(this.model.attributes));
+            var $q = $('#question-'+this.model.id, this.$el);
+            if($q.length === 0){
+                this.$el.append(template(this.model.attributes));
+            }
             return this;
         },
 
